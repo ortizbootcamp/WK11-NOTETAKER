86e53ca327acc44fbe0afc91ab2d8f1ee06c6b8e
Natasha Ortiz <tashaaortiz@gmail.com>
2021-10-15 22:04:03 -0500

    Added files

---
+++ Develop/api.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const fs = require("fs");
+const util = require("util");
+
+const fileRead = util.promisify(fs.readFile);
+const fileWrite = util.promisify(fs.writeFile);
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        (fileRead("./Develop/db/debugger.json", "utf8"))
+        .then(listNotes => {
+            res.json(JSON.parse(listNotes))
+        })
+        .catch(err => console.log(err))
+    });
+
+    app.post("/api/notes", async function (req, res) {
+        let writeNew = req.body;
+        let numb = Math.floor(Math.random() * 100 +1);
+
+        let nPr = num.toString();
+        let prop = {"id": nPr}
+        let nObj = [{"title": req.body.title, "text": req.body.text, ...prop}]
+        let nString = await fileRead("./Develop/db/db.json", "utf8");
+
+        let notes = JSON.parse(nString);
+
+        notes.push(nObj[0])
+        let nNote = JSON.stringify(notes)
+        
+        await fileWrite("./Develop/db/db.json", nNote, "utf8");
+
+        res.json(nNote);
+    });
+
+    app.delete("./api/notes/:id", async function (req, res) {
+        let noteId = req.params.id;
+        let allStr = await fileRead("./Develop/db/db.json", "utf8");
+
+        let notes = JSON.parse(allStr);
+
+        dNote = notes.filter(dnote => {
+            return dnote.id !== noteId;
+        });
+
+        let nNote = JSON.stringify(dNote)
+
+
+        await fileWrite("./Develop/db/db.json", nNote, "utf8");
+
+        res.send("Disapearroo'd!");
+    })
+
+}
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+require("./Develop/api.js")(app);
+require("./Develop/html.js")(app);
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname + "/Develop/public")));
+
+app.listen(PORT, function() {
+    console.log("Listening on port..." + PORT);
+});
+
